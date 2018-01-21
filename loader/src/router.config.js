737cb7ac28e69a07ad032cfe0131c2d2291eb191
Administrator <631926331@qq.com>
2018-01-21 15:45:22 +0800

    loader

--- loader/src/router.config.js
+++ loader/src/router.config.js
@@ -1,9 +1,9 @@
-import Home from "./component/Home.vue"
-import News from "./component/News.vue"
+import Home from "./component/Home.vue"//导入home组件
+import News from "./component/News.vue"//导入news组件
 
 export default {
   routes:[
     {path:"/home",component:Home},
     {path:"/news",component:News}
   ]
-}
+}//导出配置
